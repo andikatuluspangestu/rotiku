085b6cd5f027c9b0dac72e27095bed90f51635a6
andikatuluspangestu <andikatulusp@gmail.com>
2024-05-04 22:10:01 +0700

    Update AdminOrderController with new methods for order management

--- app/Http/Controllers/Admin/AdminOrderController.php
+++ app/Http/Controllers/Admin/AdminOrderController.php
@@ -14,7 +14,10 @@ class AdminOrderController extends Controller
     public function index()
     {
         $title  = 'Daftar Pesanan';
-        $orders = Order::orderBy('created_at', 'desc')->get();
+
+        // Tampilkan Data Order berdasarkan created_at terbaru dan hanya yang memiliki accepted_status = accepted
+        $orders = Order::where('accepted_status', 'pending')->orderBy('created_at', 'DESC')->get();
+
         return view('admin.orders.index', compact('title', 'orders'));
     }
 
@@ -23,13 +26,13 @@ class AdminOrderController extends Controller
     {
         $title  = 'Detail Pesanan';
         $order  = Order::findOrFail($id);
-        
+
         // Ambil nilai product_id dari data order di atas
         $product = $order->products;
-    
+
         return view('admin.orders.show', compact('title', 'order', 'product'));
     }
-    
+
 
     // Proses Konfirmasi Pembayaran
     public function confirmPayment(Request $request, $id)
@@ -44,4 +47,53 @@ class AdminOrderController extends Controller
         return redirect()->back()->with('success', 'Konfirmasi pembayaran berhasil.');
     }
 
+    // Ubah status pengirman
+    public function updateShipping(Request $request, $id)
+    {
+        // Cek data pesanan berdasarkan ID Order 
+        $order = Order::where('id', $id)->first();
+
+        $order->update([
+            'shipping_status' => $request->shipping_status,
+        ]);
+
+        if ($request->shipping_status == 'shipping') {
+            // Ambil Data Quantity dari Order
+            $quantity = $order->quantity;
+
+            // Ambil Data Produk berdasarkan ID Produk
+            $product = Product::where('id', $order->product_id)->first();
+
+            // Update Stok Produk
+            $productStok = $product->stock - $quantity;
+
+            $product->update([
+                'stock' => $productStok,
+            ]);
+        }
+
+        return redirect()->back()->with('success', 'Status pengiriman berhasil diubah.');
+    }
+
+    // Riwayat Pesanan
+    public function history()
+    {
+        $title  = 'Riwayat Pesanan';
+        $orders = Order::where('accepted_status', 'accepted')->orderBy('created_at', 'DESC')->get();
+
+        return view('admin.orders.history', compact('title', 'orders'));
+    }
+
+    // updateAdminNotes
+    public function updateAdminNotes(Request $request, $id)
+    {
+        // Cek data pesanan berdasarkan ID Order 
+        $order = Order::where('id', $id)->first();
+
+        $order->update([
+            'admin_notes' => $request->admin_notes,
+        ]);
+
+        return redirect()->back()->with('success', 'Catatan Admin berhasil diubah.');
+    }
 }

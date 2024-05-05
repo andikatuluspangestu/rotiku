4fd04cbb618ff7b4fde1faffeccf4ded10a1986d
andikatuluspangestu <andikatulusp@gmail.com>
2024-05-05 20:43:27 +0700

    Update user authentication logic to use bcrypt hashing algorithm

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -8,75 +8,109 @@
         <section class="row">
             <div class="col-12 col-lg-9">
                 <div class="row">
-                    <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card shadow-sm">
-                            <div class="card-body px-4 py-4-5">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                                        <div class="mb-3 mt-2 fs-4">
-                                            <i class="fa-solid fa-box-open"></i>
-                                        </div>
+                    <div class="col-6 col-lg-4 col-md-6">
+                        <div class="card bg-primary shadow-sm">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div class="avatar avatar-xl bg-primary">
+                                        {{-- Box --}}
+                                        <i class="fa-solid fa-box-open"></i>
                                     </div>
-                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="font-extrabold mb-2">22</h6>
-                                        <h6 class="text-muted font-semibold">Produk</h6>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card shadow-sm">
-                            <div class="card-body px-4 py-4-5">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                                        <div class="mb-3 mt-2 fs-4">
-                                            <i class="fa-solid fa-tags"></i>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="font-extrabold mb-2">12</h6>
-                                        <h6 class="text-muted font-semibold">Katalog</h6>
+                                    <div class="ms-3">
+                                        <h5 class="font-bold">
+                                            {{ $products->count() }}
+                                        </h5>
+                                        <h6 class="text-muted mb-0">
+                                            Total Produk
+                                        </h6>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card shadow-sm">
-                            <div class="card-body px-4 py-4-5">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                                        <div class="mb-3 mt-2 fs-4">
-                                            <i class="fa-solid fa-basket-shopping"></i>
-                                        </div>
+                    <div class="col-6 col-lg-4 col-md-6">
+                        <div class="card bg-secondary shadow-sm">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div class="avatar avatar-xl bg-secondary">
+                                        {{-- Tag --}}
+                                        <i class="fa-solid fa-tag"></i>
                                     </div>
-                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="font-extrabold mb-2">10</h6>
-                                        <h6 class="text-muted font-semibold">Pesanan</h6>
+                                    <div class="ms-3">
+                                        <h5 class="font-bold">
+                                            {{ $categories->count() }}
+                                        </h5>
+                                        <h6 class="text-muted mb-0">
+                                            Jumlah Katalog
+                                        </h6>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card shadow-sm">
-                            <div class="card-body px-4 py-4-5">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                                        <div class="mb-3 mt-2 fs-4">
-                                            <i class="fa-solid fa-star"></i>
-                                        </div>
+                    <div class="col-6 col-lg-4 col-md-6">
+                        <div class="card bg-danger shadow-sm">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div class="avatar avatar-xl bg-danger">
+                                        {{-- Shop Bag --}}
+                                        <i class="fa-solid fa-shopping-bag"></i>
                                     </div>
-                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="font-extrabold mb-2">17</h6>
-                                        <h6 class="text-muted font-semibold">Ulasan</h6>
+                                    <div class="ms-3">
+                                        <h5 class="font-bold">
+                                            {{ $orders->count() }}
+                                        </h5>
+                                        <h6 class="text-muted mb-0">
+                                            Jumlah Pesanan
+                                        </h6>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
+                {{-- Table Riwayat Pesanan --}}
+                <div class="card shadow-sm">
+                    <div class="card-header">
+                        <h5>Riwayat Pesanan</h5>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered" id="dataTable">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Tanggal</th>
+                                        <th>Produk</th>
+                                        <th>Jumlah</th>
+                                        <th>Total</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($orders as $order)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $order->created_at->format('d M Y') }}</td>
+                                            <td>
+                                                @foreach ($products as $product)
+                                                    @if ($product->id == $order->product_id)
+                                                        <span class="badge bg-secondary">{{ $product->name }}</span>
+                                                    @endif
+                                                @endforeach
+                                            </td>
+                                            <td>
+                                                {{ $order->quantity }}
+                                            </td>
+                                            <td>Rp {{ number_format($order->total) }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
             </div>
             <div class="col-12 col-lg-3">
                 {{-- Status Profile Card --}}
@@ -84,39 +118,59 @@
                     <div class="card-body py-4 px-4">
                         <div class="d-flex align-items-center">
                             <div class="avatar avatar-xl">
-                                <img src="https://cdn-icons-png.flaticon.com/512/706/706830.png" alt="avatar" class="avatar-img rounded-circle">
+                                <img src="https://cdn-icons-png.flaticon.com/512/706/706830.png" alt="avatar"
+                                    class="avatar-img rounded-circle">
                             </div>
                             <div class="ms-3 name">
                                 <h5 class="font-bold">Renata Angelin</h5>
-                                <h6 class="text-muted mb-0">@rnata</h6>
+                                <h6 class="text-muted mb-0">
+                                    {{-- Edit Profil --}}
+                                    <a href="/">Edit Profil</a>
+                                </h6>
                             </div>
                         </div>
-                    </div>
-                    <div class="desc pb-4 pt-2 px-4">
-                        You are login as Pelanggan <br> 
-                        {{-- Logout Form --}}
-                        <form action="{{ route('logout') }}" method="POST">
-                            @csrf
-                            <button type="submit" class="btn btn-sm mt-2 btn-danger">Logout</button>
-                        </form>
                     </div>
                 </div>
 
-                {{-- Card Detail Keuangan (pengeluaran) --}}
-                <div class="card shadow-sm">
-                    <div class="card-body py-4 px-4">
+                {{-- Card Total Pembelian --}}
+                <div class="card bg-primary shadow-sm">
+                    <div class="card-body">
                         <div class="d-flex align-items-center">
-                            <div class="stats-icon green">
-                                {{-- Arrow Up Iconly --}}
-                                <i class="iconly-boldWallet"></i>
+                            <div class="avatar avatar-xl bg-primary">
+                                <i class="fa-solid fa-wallet"></i>
                             </div>
                             <div class="ms-3">
-                                <h6 class="font-bold">Pengeluaran</h6>
-                                <h6 class="text-muted mb-0">Rp. 500.000</h6>
+                                <h5 class="font-bold">Total Pembelian</h5>
+                                <h6 class="text-muted mb-0">
+                                    Rp {{ number_format($totalOrder) }}
+                                </h6>
                             </div>
                         </div>
                     </div>
                 </div>
+
+                {{-- Card data product yang paling banyak dibeli oleh user --}}
+                <div class="card bg-success shadow-sm">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center">
+                            <div class="avatar avatar-xl">
+                                <i class="fa-solid fa-star"></i>
+                            </div>
+                            <div class="ms-3">
+                                <h5 class="font-bold">Produk Favorit</h5>
+                                <h6 class="text-muted mb-0">
+                                    @foreach ($products as $product)
+                                        @if ($product->id == $mostProductId)
+                                            {{ $product->name }}
+                                        @endif
+                                    @endforeach
+                                </h6>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </section>
     </div>

f7a69516d696af8f4683d5c3a5ffe049f136b946
andikatuluspangestu <andikatulusp@gmail.com>
2024-05-06 12:52:45 +0700

    Add HomeController with index method

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    // Home Page 
+    public function index()
+    {
+        return view('home.index');
+    }
+}

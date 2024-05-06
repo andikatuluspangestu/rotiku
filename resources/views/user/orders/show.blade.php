6d9885b9a4c3abc5c90b7a8d5341f0f916457464
andikatuluspangestu <andikatulusp@gmail.com>
2024-05-06 12:35:55 +0700

    Update payment information display in order show view

--- resources/views/user/orders/show.blade.php
+++ resources/views/user/orders/show.blade.php
@@ -161,8 +161,12 @@
                                 <table class="table table-bordered">
                                     <tr>
                                         <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                                            <strong>Holy guacamole!</strong> You should check in on some of those fields
-                                            below.
+                                            <strong>Cara Pembayaran!</strong>
+
+                                            <p>
+                                                {{ $settings->payment }}
+                                            </p>
+
                                             <button type="button" class="btn-close" data-bs-dismiss="alert"
                                                 aria-label="Close"></button>
                                         </div>

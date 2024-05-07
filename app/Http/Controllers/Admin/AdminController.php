704aa9b707d925310abf549946ba2830d48296dd
andikatuluspangestu <andikatulusp@gmail.com>
2024-05-07 11:06:19 +0700

    Add data retrieval and display logic to AdminController

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,18 +3,36 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\Order;
+use App\Models\Income;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $title = 'Admin Dashboard';
 
-        return view('admin.dashboard', compact('title'));
-    }
+        // Ambil data user, product, catgories dan order
+        $users      = User::all();
+        $products   = Product::all();
+        $orders     = Order::all();
+        $categories = Category::all();
 
+        // Data Pesanan Selesai
+        $ordersCompleted = Order::where('shipping_status', 'completed')->get();
+
+        // Data Pemasukan (jumlah amount keseluruhan)
+        $incomes = Income::all();
+
+        $ordersPerMonth = Order::selectRaw('MONTH(created_at) as month, COUNT(id) as total')
+        ->groupBy('month')
+        ->get();
+
+        // Tamplikan view admin.index
+        return view('admin.dashboard', compact('title', 'users', 'products', 'orders', 'categories', 'ordersCompleted', 'incomes', 'ordersPerMonth'));
+    }
 }

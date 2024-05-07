3da1aea83f0923d3b54e4717ad6a1882b89c6e3a
andikatuluspangestu <andikatulusp@gmail.com>
2024-05-07 11:07:01 +0700

    Fix bug in login functionality

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -18,7 +18,10 @@
                                         </div>
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="font-extrabold mb-2">22</h6>
+                                        <h6 class="font-extrabold mb-2">
+                                            {{-- Ambil Data Jumlah Produk --}}
+                                            {{ $products->count() }}
+                                        </h6>
                                         <h6 class="text-muted font-semibold">Produk</h6>
                                     </div>
                                 </div>
@@ -35,7 +38,10 @@
                                         </div>
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="font-extrabold mb-2">12</h6>
+                                        <h6 class="font-extrabold mb-2">
+                                            {{-- Ambil Data Jumlah Kategori --}}
+                                            {{ $categories->count() }}
+                                        </h6>
                                         <h6 class="text-muted font-semibold">Katalog</h6>
                                     </div>
                                 </div>
@@ -52,7 +58,10 @@
                                         </div>
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="font-extrabold mb-2">10</h6>
+                                        <h6 class="font-extrabold mb-2">
+                                            {{-- Ambil Data Jumlah Pesanan --}}
+                                            {{ $orders->count() }}
+                                        </h6>
                                         <h6 class="text-muted font-semibold">Pesanan</h6>
                                     </div>
                                 </div>
@@ -65,12 +74,15 @@
                                 <div class="row">
                                     <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
                                         <div class="mb-3 mt-2 fs-4">
-                                            <i class="fa-solid fa-star"></i>
+                                            <i class="fa-solid fa-check"></i>
                                         </div>
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="font-extrabold mb-2">17</h6>
-                                        <h6 class="text-muted font-semibold">Ulasan</h6>
+                                        <h6 class="font-extrabold mb-2">
+                                            {{-- ordersCompleted --}}
+                                            {{ $ordersCompleted->count() }}
+                                        </h6>
+                                        <h6 class="text-muted font-semibold">Pesanan Selesai</h6>
                                     </div>
                                 </div>
                             </div>
@@ -78,15 +90,32 @@
                     </div>
                 </div>
 
-                {{-- Grafik Keuangan --}}
+                {{-- Grafik --}}
                 <div class="row">
-                    <div class="col-12">
+                    <div class="col-6">
+
+                        {{-- Card Grafik Keuangan --}}
                         <div class="card shadow-sm">
                             <div class="card-header">
-                                <h4>Keuangan</h4>
+                                <h4>
+                                    Pemasukan Tahun {{ date('Y') }}
+                                </h4>
                             </div>
                             <div class="card-body">
-                                <canvas id="myChart" width="400" height="150"></canvas>
+                                <canvas id="moneyChart" width="500" height="180"></canvas>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col-6">
+                        {{-- Card Grafik Penjualan --}}
+                        <div class="card shadow-sm">
+                            <div class="card-header">
+                                <h4>Penjualan</h4>
+                            </div>
+                            <div class="card-body">
+                                <canvas id="orderChart" width="500" height="180"></canvas>
                             </div>
                         </div>
                     </div>
@@ -95,24 +124,22 @@
             <div class="col-12 col-lg-3">
                 {{-- Status Profile Card --}}
                 <div class="card shadow-sm">
-                    <div class="card-body py-4 px-4">
+                    <div class="card-body pt-4 px-4">
                         <div class="d-flex align-items-center">
                             <div class="avatar avatar-xl">
-                                <img src="https://cdn-icons-png.flaticon.com/512/706/706830.png" alt="avatar" class="avatar-img rounded-circle">
+                                <img src="https://cdn-icons-png.flaticon.com/512/706/706830.png" alt="avatar"
+                                    class="avatar-img rounded-circle">
                             </div>
                             <div class="ms-3 name">
-                                <h5 class="font-bold">Yuly Kurnia</h5>
-                                <h6 class="text-muted mb-0">@krnwt</h6>
+                                <h5 class="font-bold">
+                                    {{-- Ambil Data Nama Admin --}}
+                                    {{ Auth::user()->name }}
+                                </h5>
+                                <a href="">
+                                    <h6 class="mb-0">Edit Profil</h6>
+                                </a>
                             </div>
                         </div>
-                    </div>
-                    <div class="desc pb-4 pt-2 px-4">
-                        You are login as Admin <br> 
-                        {{-- Logout Form --}}
-                        <form action="{{ route('logout') }}" method="POST">
-                            @csrf
-                            <button type="submit" class="btn btn-sm mt-2 btn-danger">Logout</button>
-                        </form>
                     </div>
                 </div>
 
@@ -126,23 +153,28 @@
                             </div>
                             <div class="ms-3">
                                 <h6 class="font-bold">Pemasukan</h6>
-                                <h6 class="text-muted mb-0">Rp. 1.000.000</h6>
+                                <h6 class="text-muted mb-0">
+                                    Rp {{ number_format($incomes->sum('amount'), 0, ',', '.') }}
+                                </h6>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                {{-- Card Detail Keuangan (pengeluaran) --}}
+                {{-- Card Jumlah Pelanggan --}}
                 <div class="card shadow-sm">
                     <div class="card-body py-4 px-4">
                         <div class="d-flex align-items-center">
-                            <div class="stats-icon green">
-                                {{-- Arrow Up Iconly --}}
-                                <i class="iconly-boldWallet"></i>
+                            <div class="stats-icon blue">
+                                {{-- User Iconly --}}
+                                <i class="iconly-boldUser"></i>
                             </div>
                             <div class="ms-3">
-                                <h6 class="font-bold">Pengeluaran</h6>
-                                <h6 class="text-muted mb-0">Rp. 500.000</h6>
+                                <h6 class="font-bold">Pelanggan</h6>
+                                <h6 class="text-muted mb-0">
+                                    {{-- Jumlah user dengan role user --}}
+                                    {{ $users->where('role', 'user')->count() }}
+                                </h6>
                             </div>
                         </div>
                     </div>
@@ -153,33 +185,93 @@
         {{-- Chart.Js CDN --}}
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-        {{-- Chart.Js Dummy --}}
+        {{-- Chart Pemasukan setiap Bulan --}}
         <script>
-            var ctx = document.getElementById('myChart').getContext('2d');
+            var ctx = document.getElementById('moneyChart').getContext('2d');
+            var moneyChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: [
+                        @foreach ($incomes->sortBy('created_at') as $income)
+                            // income_at perbulan
+                            '{{ $income->created_at->format('M') }}',
+                        @endforeach
+                    ],
+                    datasets: [{
+                        label: 'Pemasukan',
+                        data: [
+                            @foreach ($incomes as $income)
+                                {{ $income->amount }},
+                            @endforeach
+                        ],
+                        backgroundColor: 'rgba(0, 123, 255, 0.1)',
+                        borderColor: 'rgba(0, 123, 255, 1)',
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+        </script>
+
+        {{-- Chart Penjualan setiap Bulan --}}
+        <script>
+            var ctx = document.getElementById('orderChart').getContext('2d');
+
+            // Menghitung jumlah bulan antara Mei dan bulan terakhir dalam data
+            @if (isset($ordersPerMonth[0]->month))
+                var totalMonths = 12 - {{ $ordersPerMonth[0]->month }} + 5;
+            @else
+                var totalMonths = 12;
+            @endif
+
+            // Membuat array kosong untuk label bulan dan data grafik
+            var labels = [];
+            var data = [];
+
+            // Menambahkan label bulan dan data grafik untuk bulan-bulan sebelum Mei dengan nilai nol
+            @php
+                $month = isset($ordersPerMonth[0]->month) ? $ordersPerMonth[0]->month : 0;
+            @endphp
+
+            @for ($i = 0; $i < $month - 1; $i++)
+                labels.push('');
+                data.push(0);
+            @endfor
+
+            // Menambahkan label bulan dan data grafik untuk bulan Mei hingga bulan terakhir dalam data
+            @foreach ($ordersPerMonth as $order)
+                labels.push('{{ date('M', mktime(0, 0, 0, $order->month, 1)) }}');
+                data.push({{ $order->total }});
+            @endforeach
+
+            // Mengganti label bulan menjadi nama bulan yang sesuai
+            for (var i = 0; i < labels.length; i++) {
+                if (labels[i] === '') {
+                    var monthIndex = i % 12; // Menentukan indeks bulan berdasarkan indeks iterasi
+                    var monthName = new Date(null, monthIndex).toLocaleDateString('en', {
+                        month: 'long'
+                    }); // Mengonversi indeks bulan menjadi nama bulan
+                    labels[i] = monthName;
+                }
+            }
+
+
             var myChart = new Chart(ctx, {
                 type: 'line',
                 data: {
-                    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                    labels: labels,
                     datasets: [{
-                        label: 'Pemasukan',
-                        data: [12, 19, 3, 5, 2, 3, 5, 7, 8, 9, 10, 11],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                        ],
-                        borderWidth: 1
-                    }, {
-                        label: 'Pengeluaran',
-                        data: [1, 2, 1, 1, 2, 2, 1, 1, 2, 2, 1, 1],
-                        backgroundColor: [
-                            'rgba(54, 162, 235, 0.2)',
-                        ],
-                        borderColor: [
-                            'rgba(54, 162, 235, 1)',
-                        ],
-                        borderWidth: 1
+                        label: 'Orders per Month',
+                        data: data,
+                        fill: false,
+                        borderColor: 'rgb(75, 192, 192)',
+                        tension: 0.1
                     }]
                 },
                 options: {
